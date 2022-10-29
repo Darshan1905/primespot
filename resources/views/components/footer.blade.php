5982b862ec813884ee0acb68bfb2a71b97119492
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-10-29 18:27:27 +0530

    Update footer.blade.php
    update

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,75 +1,74 @@
 <footer id="footer" class="clearfix main-foot-sec pt-5 pb-5">
-        <div id="footer-widgets" class="container">
-          <div class="row">
-            <div class="col-lg-5 col-md-8 pt-4 ">
-              <!-- <div class="widget"> -->
-              <div class="text-center">
-                <a href="index" rel="home" class="main-logo">
-                  <img src="assets/images/logo/primespot-logo-01.svg" alt="images" style="width: 30%;" />
-                </a>
-              </div>
-              <h5 class="text-color-4 foot-data footer-section-1 p-3">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Necessitatibus quasi, dolores inventore voluptatem laudantium
-                libero commodi deleniti quaerat sequi, porro doloremque, ab
-                repellat dolor laboriosam iste ea quas nam eum?
-              </h5>
+<div id="footer-widgets" class="container">
+<div class="row">
+<div class="col-lg-5 col-md-8 pt-4 ">
+<!-- <div class="widget"> -->
+<div class="text-center">
+<a href="index" rel="home" class="main-logo">
+  <img src="assets/images/logo/primespot-logo-01.svg" alt="images" style="width: 30%;" />
+</a>
+</div>
+<h5 class="text-color-4 foot-data footer-section-1 p-3">
+Lorem ipsum dolor sit amet consectetur adipisicing elit.
+Necessitatibus quasi, dolores inventore voluptatem laudantium
 
-              <!-- </div> -->
-            </div>
-            <div class="col-lg-2 col-md-4 pt-5 ">
-              <!-- <div class="footer-widgets-nav-menu"> -->
-              <!-- <div class="menu-1"> -->
+</h5>
 
-              <ul class="link-wrap">
-                <h3 class="widget-title-link-wrap text-color-3">
-                  Quick Links
-                </h3>
-                <li><a href="company">Company</a></li>
-                <li><a href="solution">Solution</a></li>
-                <li><a href="integration">Integration</a></li>
-                <li><a href="clientele">Clientele</a></li>
-                <li><a href="career">Career</a></li>
-              </ul>
+<!-- </div> -->
+</div>
+<div class="col-lg-2 col-md-4 pt-5 ">
+<!-- <div class="footer-widgets-nav-menu"> -->
+<!-- <div class="menu-1"> -->
 
-              <!-- </div> -->
-              <!-- </div> -->
-            </div>
-            <div class="col-lg-3 col-md-8  footer-second-section pt-5 ">
-              <!-- <div class="footer-widgets-nav-menu"> -->
-              <!-- <div class="menu-1"> -->
-              <ul class=" footer-section-3 style-link">
-                <h3 class="widget-title-link-wrap text-color-3">
-                  Contact Us
-                </h3>
-                <h5 class="text-color-4 foot-data2">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Necessitatibus quasi, porro doloremque, ab repellat dolor
-                  laboriosam iste ea quas nam eum?
-                </h5>
-              </ul>
-              <!-- </div> -->
-              <!-- </div> -->
-            </div>
-            <div class="col-lg-2 col-md-4 footer-second-section pt-5 ">
-              <!-- <div class="menu-2"> -->
-              <h3 class="widget-title-link-wrap text-color-3">
-                Social Media
-              </h3>
-              <div class="social-icon mt-4">
-                <div >
-                  <img src="/assets/images/image-box/fb.png" alt="" style="width: 70%;">
+<ul class="link-wrap">
+<h3 class="widget-title-link-wrap text-color-3">
+  Quick Links
+</h3>
+<li><a href="company">Company</a></li>
+<li><a href="solution">Solution</a></li>
+<li><a href="integration">Integration</a></li>
+<li><a href="clientele">Clientele</a></li>
+<li><a href="career">Career</a></li>
+</ul>
+
+<!-- </div> -->
+<!-- </div> -->
+</div>
+<div class="col-lg-3 col-md-8  footer-second-section pt-5 ">
+<!-- <div class="footer-widgets-nav-menu"> -->
+<!-- <div class="menu-1"> -->
+<ul class=" footer-section-3 style-link">
+<h3 class="widget-title-link-wrap text-color-3">
+  Contact Us
+</h3>
+<h5 class="text-color-4 foot-data2">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Necessitatibus quasi, porro doloremque, ab repellat dolor
+  laboriosam iste ea quas nam eum?
+</h5>
+</ul>
+<!-- </div> -->
+<!-- </div> -->
+</div>
+<div class="col-lg-2 col-md-4 footer-second-section pt-5 ">
+<!-- <div class="menu-2"> -->
+<h3 class="widget-title-link-wrap text-color-3">
+Social Media
+</h3>
+<div class="social-icon mt-4">
+<div >
+  <img src="/assets/images/image-box/fb.png" alt="" style="width: 70%;">
+</div>
+<div >
+  <img src="/assets/images/image-box/insta.png" alt="" style="width: 70%;">
+</div>
+<div >
+  <img src="/assets/images/image-box/wp.png" alt="" style="width: 70%;">
                 </div>
-                <div >
-                  <img src="/assets/images/image-box/insta.png" alt="" style="width: 70%;">
-                </div>
-                <div >
-                  <img src="/assets/images/image-box/wp.png" alt="" style="width: 70%;">
-                                </div>
-              </div>
-              
-              <!-- </div> -->
-            </div>
-          </div>
-        </div>
-      </footer>
+</div>
+
+<!-- </div> -->
+</div>
+</div>
+</div>
+</footer>

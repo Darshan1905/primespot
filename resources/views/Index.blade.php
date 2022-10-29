207600060192b3d406532e39f9e56171194f1de8
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-10-29 11:50:21 +0530

    Update Index.blade.php
    text update

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -232,7 +232,7 @@
                   </div>
                 <div class="trusted-heading">
                   <!-- <div class="tf-sub-title">Please Get To Know Us</div> -->
-                  <h2 class="tf-title">
+                  <!--<h2 class="tf-title">
                     We Are
                     <span class="text-color-3 style-title">Trusted</span> By
                     First & Best In The World.
@@ -241,13 +241,23 @@
                 <h3 class="text-color-3">
                   Hello pretium nunc. Aliquam rutrum lectus vel est
                   pulvinar, in scelerisque purus faucibus fusce varius lacinia.
-                </h3>
+                </h3>-->
+                
+                
                 <p>
-                  Phasellus dignissim arcu sit amet augue mattis, eget rutrum ex
-                  finibus. Morbi blandit luctus nisi, id ornare sem blandit sed.
-                  In sed luctus dolor. Integer vitae pretium nunc. Aliquam
-                  rutrum lectus vel est pulvinar in scelerisque purus.
+                  PrimeSpot specializes in integrated e-commerce solutions for brands with special expertise in  premium and luxury brands. 
+                  After tasting success with over 100+ brands like Bvlgari, Chopard, Hermes, Guess, DKNY, CK, Polaroid and many more in the fragrance, 
+                  beauty, luggage & sunglass categories, PrimeSpot set out to service other organizations keeping in mind the highest standards 
+                  of efficiency and networking to enable brands to tap into the booming business vertical of e-commerce. With a host of 
+                  services like <span  class="text-color-3 style-title">content generation and management for e-commerce portals, state of the art 
+                  warehousing capabilities, marketplace negotiation & management, logistics and fulfillment, returns and claims processing, 
+                  financial reconciliation;</span> PrimeSpot has several business models that it operates on to empower its clients to 
+                  choose just what’s right for their business by being fully risk-return aligned. 
                 </p>
+                
+                
+                
+                
                 <div class="content-progress-box">
                   <div class="progress-bar" data-percentage="80%">
                     <h4 class="progress-title-holder">

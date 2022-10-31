e95a123dbcbc9abb9f0490940e115a0a79c2a286
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-10-31 10:26:50 +0530

    Update Career.blade.php
    code comment

--- resources/views/Career.blade.php
+++ resources/views/Career.blade.php
@@ -1,212 +1,195 @@
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
 
+<meta charset="utf-8" /><title>Career</title>
+<meta name="author" content="themesflat.com" />
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-  <head>
+<!-- Theme Style -->
+<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
 
-    <meta charset="utf-8" />
-    <title>Career</title>
+<!-- Swiper slide -->
+<link rel="stylesheet" type="text/css" href="assets/css/swiper-bundle.min.css"/>
 
-    <meta name="author" content="themesflat.com" />
+<!-- Reponsive -->
+<link rel="stylesheet" type="text/css" href="assets/css/responsive.css" />
 
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+<!-- Favicon and Touch Icons  -->
+<link rel="shortcut icon" href="assets/icon/Favicon.png" />
+<link rel="apple-touch-icon-precomposed" href="assets/icon/Favicon.png" />
+</head>
 
-     <meta 
-    name="viewport" 
-    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<body class="header-fixed">
 
+<div id="wrapper" class="animsition">
+<div id="page" class="clearfix">
+<!-- Header Wrap -->
+<div id="site-header-wrap">
+<!-- /#top-bar -->
+<!-- Header -->
+<header id="site-header">
+<div id="site-logo" class="clearfix">
+<div id="site-log-inner">
+<a href="/" rel="home" class="main-logo">
+<img src="assets/images/logo/primespot-logo-01.svg" alt="bixos" width="200" height="150" data-width="100" data-height="44" style="width: 100%;"/>
+</a>
+</div>
+</div>
 
-    <!-- Theme Style -->
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+<div class="mobile-button">
+<span></span>
+</div>
+<!-- /.mobile-button -->
+<!-- /#site-logo -->
+<div id="site-header-inner">
+<div class="wrap-inner clearfix">
+<nav id="main-nav" class="main-nav">
+<ul id="menu-primary-menu" class="menu">
+<li class="menu-item">
+<a href="company">Company</a>
+<!-- <ul class="sub-menu">
+<li class="menu-item current-item"><a href="company">Company01</a></li>
+</ul> -->
+</li>
+<li class="menu-item">
+<a href="solution">Solution</a>
+<!-- <ul class="sub-menu">
+<li class="menu-item"><a href="solution">Solution01</a></li>
+</ul> -->
+</li>
+<li class="menu-item">
+<a href="integration">Integration</a>
+<!-- <ul class="sub-menu">
+<li class="menu-item"><a href="integration">Integration01</a></li>
+</ul> -->
+</li>
+<li class="menu-item">
+<a href="clientele">Clientele</a>
+<!-- <ul class="sub-menu">
+<li class="menu-item"><a href="clientele">Clientele01</a></li>
+</ul> -->
+</li>
+<li class="menu-item current-menu-item">
+<a href="career">Career</a>
+<!-- <ul class="sub-menu">
+<li class="menu-item"><a href="career">Career01</a></li>
+</ul> -->
+</li>
+</ul>
+</nav>
+<!-- /#main-nav -->
+</div>
+<!-- /.wrap-inner -->
+</div>
 
-    <!-- Swiper slide -->
-    <link
-      rel="stylesheet"
-      type="text/css"
-      href="assets/css/swiper-bundle.min.css"
-    />
+<!-- /#site-header-inner -->
 
-    <!-- Reponsive -->
-    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css" />
+<div class="tf-button">
+<a href="contact" class="button btn-1"><span>Contact Us </span></a>
+</div>
+</header>
+<!-- /#site-header -->
+<section class="flat-title-page">
+<div class="overlay-page"></div>
+<div class="container">
+<div class="row">
+<div class="col-lg-12 col-md-12">
+<div class="breadcrumbs">
+<h1>Career</h1>
+<div class="breadcrumb-trail link-style-2">
+<a class="home" href="/">Home</a><span>Career</span>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+</div>
+<!-- #site-header-wrap -->
 
-    <!-- Favicon and Touch Icons  -->
-    <link rel="shortcut icon" href="assets/icon/Favicon.png" />
-    <link rel="apple-touch-icon-precomposed" href="assets/icon/Favicon.png" />
-  </head>
+<div class="container career-con pt-5 pb-5 mt-5 mb-5">
+<div class="flat-trusted">
+<div class="mark-page34 tf-mark "> <img src="assets/images/mark-page/dots.svg" alt="images" style="width:70%" > </div>
+<div class="mark-page35 tf-mark animate-up"> <img src="assets/images/mark-page/red-truck.svg" alt="images" style="width:70%" > </div>
+<div class="mark-page36 tf-mark "> <img src="assets/images/mark-page/hexagone.svg" alt="images" style="width:70%" > </div>
+</div>
+<div class="row mt-5 mb-5">
+<div class="col-lg-4 col-md-4 col-sm-6 mt-4">
+<div class="box-out">
+<div class="box-in">
+<h3>Head of Delivery</h3>
+<p style="color: #000">Mumbai</p>
+<p style="color: #000">Full Time</p>
+<br/>
+<button class="btn-careere">Apply Now</button>
+</div>
+</div>
+</div>
 
-  <body class="header-fixed">
+<!--<div class="col-lg-4 col-md-4 col-sm-6 mt-4">
+<div class="box-out">
+<div class="box-in">
+<h3>Head of Delivery</h3>
+<p style="color: #000">Mumbai</p>
+<p style="color: #000">Full Time</p>
+<br />
+<button class="btn-careere">Apply Now</button>
+</div>
+</div>
+</div>
 
-    <div id="wrapper" class="animsition">
-      <div id="page" class="clearfix">
-        <!-- Header Wrap -->
-        <div id="site-header-wrap">
-          <!-- /#top-bar -->
-          <!-- Header -->
-          <header id="site-header">
-            <div id="site-logo" class="clearfix">
-              <div id="site-log-inner">
-                <a href="/" rel="home" class="main-logo">
+<div class="col-lg-4 col-md-4 col-sm-6 mt-4 career-block3">
+<div class="box-out">
+<div class="box-in">
+<h3>Head of Delivery</h3>
+<p style="color: #000">Mumbai</p>
+<p style="color: #000">Full Time</p>
+<br/>
+<button class="btn-careere">Apply Now</button>
+</div>
+</div>
+</div>-->
+</div>
 
-                  
-                  <img src="assets/images/logo/primespot-logo-01.svg" alt="bixos" width="200" height="150" data-width="100" data-height="44" style="width: 100%;"/>
-                </a>
-              </div>
-            </div>
-            <div class="mobile-button">
-              <span></span>
-            </div>
-            <!-- /.mobile-button -->
-            <!-- /#site-logo -->
-            <div id="site-header-inner">
-              <div class="wrap-inner clearfix">
-                <nav id="main-nav" class="main-nav">
-                  <ul id="menu-primary-menu" class="menu">
-                    <li class="menu-item">
-                      <a href="company">Company</a>
-                      <!-- <ul class="sub-menu">
-                      <li class="menu-item current-item"><a href="company">Company01</a></li>
+</div>
 
-                    </ul> -->
-                    </li>
-                    <li class="menu-item">
-                      <a href="solution">Solution</a>
-                      <!-- <ul class="sub-menu">
-                      <li class="menu-item"><a href="solution">Solution01</a></li>
-                    </ul> -->
-                    </li>
-                    <li class="menu-item">
-                      <a href="integration">Integration</a>
-                      <!-- <ul class="sub-menu">
-                      <li class="menu-item"><a href="integration">Integration01</a></li>
-                    </ul> -->
-                    </li>
-                    <li class="menu-item">
-                      <a href="clientele">Clientele</a>
-                      <!-- <ul class="sub-menu">
-                      <li class="menu-item"><a href="clientele">Clientele01</a></li>
-                    </ul> -->
-                    </li>
-                    <li class="menu-item current-menu-item">
-                      <a href="career">Career</a>
-                      <!-- <ul class="sub-menu">
-                      <li class="menu-item"><a href="career">Career01</a></li>
-                    </ul> -->
-                    </li>
-                  </ul>
-                </nav>
-                <!-- /#main-nav -->
-              </div>
-              <!-- /.wrap-inner -->
-            </div>
+<!-- Footer -->
+<x-footer />
+<!-- /#footer -->
 
-            <!-- /#site-header-inner -->
+<!-- Bottom -->
+<!-- <div class="widget-social-footer">
+<div class="container">
+<div>
+<h3>
+Copywright by
+<a
+href="https://themeforest.net/user/themesflat/portfolio"
+class="text-color-3"
+>@ThemesFlat</a
+>.All Rights Reserved.
+</h3>
+</div>
+</div>
+</div> -->
+</div>
+<!-- /#page -->
+</div>
+<a id="scroll-top"></a>
+<!-- /#wrapper -->
 
-            <div class="tf-button">
-              <a href="contact" class="button btn-1"
-                ><span>Contact Us </span></a
-              >
-            </div>
-          </header>
-          <!-- /#site-header -->
-          <section class="flat-title-page">
-            <div class="overlay-page"></div>
-            <div class="container">
-              <div class="row">
-                <div class="col-lg-12 col-md-12">
-                  <div class="breadcrumbs">
-                    <h1>Career</h1>
-                    <div class="breadcrumb-trail link-style-2">
-                      <a class="home" href="/">Home</a
-                      ><span>Career</span>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-        <!-- #site-header-wrap -->
+<!-- Javascript -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="assets/js/jquery.easing.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/shortcodes.js"></script>
+<script src="assets/js/jquery.fancybox.js"></script>
+<script src="assets/js/plugin.js"></script>
 
-        <div class="container career-con pt-5 pb-5 mt-5 mb-5">
-        <div class="flat-trusted">
-                  <div class="mark-page34 tf-mark "> <img src="assets/images/mark-page/dots.svg" alt="images" style="width:70%" > </div>
-                  <div class="mark-page35 tf-mark animate-up"> <img src="assets/images/mark-page/red-truck.svg" alt="images" style="width:70%" > </div>
-                  <div class="mark-page36 tf-mark "> <img src="assets/images/mark-page/hexagone.svg" alt="images" style="width:70%" > </div>
-          </div>
-          <div class="row mt-5 mb-5">
-            <div class="col-lg-4 col-md-4 col-sm-6 mt-4">
-              <div class="box-out">
-                <div class="box-in">
-                  <h3>Head of Delivery</h3>
-                  <p style="color: #000">Mumbai</p>
-                  <p style="color: #000">Full Time</p>
-                  <br />
-                  <button class="btn-careere">Apply Now</button>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 mt-4">
-              <div class="box-out">
-                <div class="box-in">
-                  <h3>Head of Delivery</h3>
-                  <p style="color: #000">Mumbai</p>
-                  <p style="color: #000">Full Time</p>
-                  <br />
-                  <button class="btn-careere">Apply Now</button>
-                </div>
-              </div>
-            </div>
-            <div class="col-lg-4 col-md-4 col-sm-6 mt-4 career-block3">
-              <div class="box-out">
-                <div class="box-in">
-                  <h3>Head of Delivery</h3>
-                  <p style="color: #000">Mumbai</p>
-                  <p style="color: #000">Full Time</p>
-                  <br />
-                  <button class="btn-careere">Apply Now</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-
-        <!-- Footer -->
-        <x-footer />
-        <!-- /#footer -->
-
-        <!-- Bottom -->
-        <!-- <div class="widget-social-footer">
-          <div class="container">
-            <div>
-              <h3>
-                Copywright by
-                <a
-                  href="https://themeforest.net/user/themesflat/portfolio"
-                  class="text-color-3"
-                  >@ThemesFlat</a
-                >.All Rights Reserved.
-              </h3>
-            </div>
-          </div>
-        </div> -->
-      </div>
-      <!-- /#page -->
-    </div>
-    <a id="scroll-top"></a>
-    <!-- /#wrapper -->
-
-    <!-- Javascript -->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-    <script src="assets/js/jquery.easing.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <script src="assets/js/main.js"></script>
-    <script src="assets/js/shortcodes.js"></script>
-    <script src="assets/js/jquery.fancybox.js"></script>
-    <script src="assets/js/plugin.js"></script>
-
-    <script src="assets/js/swiper-bundle.min.js"></script>
-    <script src="assets/js/swiper.js"></script>
-  </body>
+<script src="assets/js/swiper-bundle.min.js"></script>
+<script src="assets/js/swiper.js"></script>
+</body>
 </html>

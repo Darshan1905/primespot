f65c2e9f6c5a38a34e4855989527c1ca597246e7
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-11-01 15:04:08 +0530

    Update Index.blade.php

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -77,9 +77,9 @@
 
 </li>
 <li class="menu-item ">
-<a href="solution">Solution</a>
+<a href="solution">Solutions</a>
 <!-- <ul class="sub-menu">
-<li class="menu-item"><a href="Solutions">Solution01</a></li>
+<li class="menu-item"><a href="Solution">Solution01</a></li>
 </ul> -->
 </li>
 <li class="menu-item ">

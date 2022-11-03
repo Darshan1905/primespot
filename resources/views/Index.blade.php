f177cf04ffdb7c2a3d2ecc3f04828e25a4451491
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-11-03 13:58:10 +0530

    Update Index.blade.php

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -57,7 +57,7 @@
 <header id="site-header">
 <div id="site-logo" class="clearfix">
 <div id="site-log-inner">
-<a href="/" rel="home" class="main-logo"> <img src="assets/images/logo/primespot-logo.svg" alt="bixos" width="200" height="150" data-width="100" data-height="44" style="width: 100%;"/> </a> </div>
+<a href="/" rel="home" class="main-logo"> <img src="assets/images/logo/primespot-logo.svg" alt="bixos" width="200" height="150" data-width="100" data-height="44" style="/*width: 100%;*/"/> </a> </div>
 </div>
 <div class="mobile-button">
 <span></span>

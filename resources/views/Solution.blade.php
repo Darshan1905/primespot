d532eb6d55630c14824e81dda8b53a8368fc6e95
Narensingh85 <111748888+Narensingh85@users.noreply.github.com>
2022-11-03 14:06:28 +0530

    Update Solution.blade.php

--- resources/views/Solution.blade.php
+++ resources/views/Solution.blade.php
@@ -66,7 +66,7 @@ crossorigin="anonymous" referrerpolicy="no-referrer" />
 <div id="site-logo" class="clearfix">
 <div id="site-log-inner">
 <a href="/" rel="home" class="main-logo">
-<img src="assets/images/logo/primespot-logo.svg" alt="bixos" width="200" height="150" data-width="100" data-height="44" style="width: 100%;"/>
+<img src="assets/images/logo/primespot-logo.svg" alt="bixos" width="200" height="150" data-width="100" data-height="44" style="/*width: 100%;*/"/>
 </a>
 </div>
 </div>
